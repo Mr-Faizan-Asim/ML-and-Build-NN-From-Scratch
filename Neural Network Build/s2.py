cb3bf4ff7e70556c867f8ba544f4ad0fe181b8e4
Mr-Faizan-Asim <mr.faizan.asim@gmail.com>
2024-06-19 00:03:55 +0500

    Created Layer Dense Class of NN

--- Neural Network Build/s2.py
+++ Neural Network Build/s2.py
@@ -5,7 +5,23 @@ X = [[3, 6, 8],
          [-1.5,2.7,3.3]]
 
 class Layer_Dense:
-    def __init__(self):
-        pass
-    def forward(self):
-        pass
+    def __init__(self,n_inputs,n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs,n_neurons)
+        self.biases = np.zeros((1,n_neurons))
+    def forward(self,input):
+        self.output = np.dot(input,self.weights) + self.biases
+
+
+
+layer1  = Layer_Dense(3,3)
+layer1.forward(X)
+print(layer1.output)
+
+print("2nd")
+layer2 = Layer_Dense(3,5)
+layer2.forward(X)
+print(layer2.output)
+
+
+
+
