1a4d7723f0f357f6745a3efad897e0f9dc4c38ef
Mr-Faizan-Asim <mr.faizan.asim@gmail.com>
2024-06-18 23:46:27 +0500

    Layer_Dense Class

---
+++ Neural Network Build/s2.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+X = [[3, 6, 8],
+         [2,5.5,-3],
+         [-1.5,2.7,3.3]]
+
+class Layer_Dense:
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
