c3fdefb694ee8f71020ba7a5f25eae17035a33d3
Mr-Faizan-Asim <mr.faizan.asim@gmail.com>
2024-06-18 23:13:13 +0500

    Learn BAtches

--- Neural Network Build/s1.py
+++ Neural Network Build/s1.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-input = [3, 6, 8]
+input = [[3, 6, 8],
+         [2,5.5,-3],
+         [-1.5,2.7,3.3]]
 weights = [
     [1.1, 3.01, 2.3],
     [4.1, -2.1, 4.2],
@@ -10,4 +12,4 @@ biases = [3, 0.5, 2]
 
 output =  np.dot(input,weights) + biases
 
-print(sum(output))
+print(output)
