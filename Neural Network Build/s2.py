7b781cdebc1917509ae14deceb470e34188b02d4
Mr-Faizan-Asim <mr.faizan.asim@gmail.com>
2024-06-22 23:55:47 +0500

    Basic Relu

--- Neural Network Build/s2.py
+++ Neural Network Build/s2.py
@@ -3,7 +3,17 @@ import numpy as np
 X = [[3, 6, 8],
          [2,5.5,-3],
          [-1.5,2.7,3.3]]
+input = [3, 6, 8,-1.5,2.7,3.3]
 
+Output = []
+for i in input:
+    if i > 0:
+        Output.append(i)
+    else:
+        Output.append(0)
+print(Output)
+
+'''
 class Layer_Dense:
     def __init__(self,n_inputs,n_neurons):
         self.weights = 0.1 * np.random.randn(n_inputs,n_neurons)
@@ -21,6 +31,7 @@ print("Next 2nd Layer")
 layer2 = Layer_Dense(3,5)
 layer2.forward(layer1.output)
 print(layer2.output)
+'''
 
 
 
